bfd2037d980d35c33950ff0d6744309d55a0f23a
shamshod01 <realshamshod01@gmail.com>
2024-05-29 14:25:35 +0900

    tg sdk

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import FlipImage from './FlipImage';
 import frontImage from './usd-front.png';
 import backImage from './back.png';
 import './App.css';
+import { SDKProvider } from '@tma.js/sdk-react';
 
 const App: React.FC = () => {
     return (
+        <SDKProvider acceptCustomStyles debug={true}>
         <div className="App">
             <FlipImage frontImage={frontImage} backImage={backImage} />
         </div>
+        </SDKProvider>
     );
 };
 
--- src/FlipImage.tsx
+++ src/FlipImage.tsx
@@ -1,7 +1,13 @@
 // src/FlipImage.tsx
 import React, { useState, useEffect, useRef } from 'react';
+import {
+    useBackButton,
+    useBackButtonRaw,
+    useViewport,
+    useViewportRaw,
+    useBiometryManagerRaw,
+} from '@tma.js/sdk-react';
 import './FlipImage.css';
-
 interface FlipImageProps {
     frontImage: string;
     backImage: string;
@@ -12,6 +18,25 @@ const FlipImage: React.FC<FlipImageProps> = ({ frontImage, backImage }) => {
     const [flipCount, setFlipCount] = useState(0);
     const [isMoving, setIsMoving] = useState(false);
     const flipSound = useRef<HTMLAudioElement | null>(null);
+// BackButton initializes synchronously. So, bb will be
+// the BackButton instance.
+    const bb = useBackButton();
+
+// Viewport is being initialized asynchronously, so signal may return undefined.
+// After some time it will receive a valid value.
+    const vp = useViewport();
+
+    useEffect(() => {
+        console.log(vp); // will be undefined and then Viewport instance.
+    }, [vp]);
+
+    const bm = useBiometryManagerRaw();
+
+    useEffect(() => {
+        if (bm.error) {
+            console.log('Something went wrong for BiometryManager', bm.error);
+        }
+    }, [bm]);
 
     useEffect(() => {
         // Initialize the audio element once the component mounts
