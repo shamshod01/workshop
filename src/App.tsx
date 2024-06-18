535eb7f0c95e4973a14431150f8a00155b38e868
tlatkdgus1 <tlatkdgus1@gmail.com>
2024-06-18 13:08:41 +0900

    tesdt

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-// src/App.tsx
-import React from 'react';
-import FlipImage from './FlipImage';
-import frontImage from './usd-front.png';
-import backImage from './back.png';
 import './App.css';
+
+// src/App.tsx
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 import { SDKProvider } from '@tma.js/sdk-react';
 
+import backImage from './back.png';
+import FlipImage from './FlipImage';
+import frontImage from './usd-front.png';
+
 const App: React.FC = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [debug, setDebug] = useState(searchParams.get('tid'));
+    useEffect(() => {
+        if (searchParams.has('tid')) {
+            setDebug(searchParams.get('tid'));
+        }
+    },[]);
     return (
         <SDKProvider acceptCustomStyles debug={true}>
-            <h1>Flip Image</h1>
+            <h1>USER TID: {debug}</h1>
+
         <div className="App">
             <FlipImage frontImage={frontImage} backImage={backImage} />
         </div>
